207c822f30784fb0fe06e257405ee720a6d12df5
故事与你 <126243018+youzhiguihaha@users.noreply.github.com>
2026-02-03 19:40:23 +0800

    Delete src directory

--- src/lib/api.ts
+++
@@ -1,8 +0,0 @@
-export const getAuthHeaders = () => {
-  const password = localStorage.getItem('site_password');
-  return {
-    'Authorization': `Bearer ${password}`,
-  };
-};
-
-export const API_BASE = '/api';
